e724753076a0fb11e3306e57d46fb6ff660d1749
Brang Pakdring <Brang.Pakdring@gmail.com>
2020-03-21 00:03:27 +0800

    Fix typo.

--- __charconv.h
+++ __charconv.h
@@ -3,7 +3,7 @@
 #if __cplusplus >= 201703
 
 // The floating point to_chars implementations is missing in current libstdc++
-// library misses.
+// library. Exclude this if they are implemented.
 namespace std {
 extern to_chars_result to_chars(char *first, char *last, float value);
 extern to_chars_result to_chars(char *first, char *last, double value);
