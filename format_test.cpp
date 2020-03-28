f3057574d61e88cc31e68a5b4b61fce48e79d368
Brang Pakdring <Brang.Pakdring@gmail.com>
2020-03-28 16:34:57 +0800

    Add more align & fill tests.

--- format_test.cpp
+++ format_test.cpp
@@ -10,9 +10,10 @@ using namespace std;
 
 struct BasicFormatTest : testing::Test {};
 struct FormatTest : testing::Test {};
+struct FillAlignShiftTest;
 struct IntegerFormatTest;
 struct FloatingPointFormatTest;
-struct UserDefinedFormatTest : FormatTest {};
+struct UserDefinedFormatTest;
 
 TEST_F(BasicFormatTest, BasicFormatTest_SimpleFormat_Test) {
   // HELLO WORLD
@@ -296,4 +297,150 @@ TEST_F(FloatingPointFormatTest, FloatingPointFormatTest_SpecialValues_Test) {
   test_special((long double){});
 }
 
-TEST_F(UserDefinedFormatTest, UserDefinedFormatTest) {}
+struct FillAlignShiftTest : FormatTest {};
+
+TEST_F(FillAlignShiftTest, FillAlignShiftTest_Decimal_Test) {
+  EXPECT_EQ(format("{:10}", 42), "        42");
+  EXPECT_EQ(format("{:`>10}", 42), "````````42");
+  EXPECT_EQ(format("{:`<10}", 42), "42````````");
+  EXPECT_EQ(format("{:`^10}", 42), "````42````");
+  EXPECT_EQ(format("{:`^11}", 42), "````42`````");
+  EXPECT_EQ(format("{:`>11}", 42), "`````````42");
+  EXPECT_EQ(format("{:`<11}", 42), "42`````````");
+
+  EXPECT_EQ(format("{:10}", -42), "       -42");
+  EXPECT_EQ(format("{:`>10}", -42), "```````-42");
+  EXPECT_EQ(format("{:`<10}", -42), "-42```````");
+  EXPECT_EQ(format("{:`^10}", -42), "```-42````");
+  EXPECT_EQ(format("{:`^11}", -42), "````-42````");
+  EXPECT_EQ(format("{:`>11}", -42), "````````-42");
+  EXPECT_EQ(format("{:`<11}", -42), "-42````````");
+}
+
+TEST_F(FillAlignShiftTest, FillAlignShiftTest_Hexadecimal_Test) {
+  EXPECT_EQ(format("{:10x}", -0x2a), "       -2a");
+  EXPECT_EQ(format("{:`>10x}", -0x2a), "```````-2a");
+  EXPECT_EQ(format("{:`<10x}", -0x2a), "-2a```````");
+  EXPECT_EQ(format("{:`^10x}", -0x2a), "```-2a````");
+  EXPECT_EQ(format("{:`^11x}", -0x2a), "````-2a````");
+  EXPECT_EQ(format("{:`>11x}", -0x2a), "````````-2a");
+  EXPECT_EQ(format("{:`<11x}", -0x2a), "-2a````````");
+
+  EXPECT_EQ(format("{:#10X}", -0x2a), "     -0X2A");
+  EXPECT_EQ(format("{:`>#10X}", -0x2a), "`````-0X2A");
+  EXPECT_EQ(format("{:`<#10X}", -0x2a), "-0X2A`````");
+  EXPECT_EQ(format("{:`^#10X}", -0x2a), "``-0X2A```");
+  EXPECT_EQ(format("{:`^#11X}", -0x2a), "```-0X2A```");
+  EXPECT_EQ(format("{:`>#11X}", -0x2a), "``````-0X2A");
+  EXPECT_EQ(format("{:`<#11X}", -0x2a), "-0X2A``````");
+
+  EXPECT_EQ(format("{: #10X}", 0x2a), "      0X2A");
+  EXPECT_EQ(format("{:`> #10X}", 0x2a), "````` 0X2A");
+  EXPECT_EQ(format("{:`< #10X}", 0x2a), " 0X2A`````");
+  EXPECT_EQ(format("{:`^ #10X}", 0x2a), "`` 0X2A```");
+  EXPECT_EQ(format("{:`^ #11X}", 0x2a), "``` 0X2A```");
+  EXPECT_EQ(format("{:`> #11X}", 0x2a), "`````` 0X2A");
+  EXPECT_EQ(format("{:`< #11X}", 0x2a), " 0X2A``````");
+
+  EXPECT_EQ(format("{:+#10X}", 0x2a), "     +0X2A");
+  EXPECT_EQ(format("{:`>+#10X}", 0x2a), "`````+0X2A");
+  EXPECT_EQ(format("{:`<+#10X}", 0x2a), "+0X2A`````");
+  EXPECT_EQ(format("{:`^+#10X}", 0x2a), "``+0X2A```");
+  EXPECT_EQ(format("{:`^+#11X}", 0x2a), "```+0X2A```");
+  EXPECT_EQ(format("{:`>+#11X}", 0x2a), "``````+0X2A");
+  EXPECT_EQ(format("{:`<+#11X}", 0x2a), "+0X2A``````");
+}
+
+TEST_F(FillAlignShiftTest, FillAlignShiftTest_Binary_Test) {
+  EXPECT_EQ(format("{:#10b}", -0b1010), "   -0b1010");
+  EXPECT_EQ(format("{:`>#10b}", -0b1010), "```-0b1010");
+  EXPECT_EQ(format("{:`<#10b}", -0b1010), "-0b1010```");
+  EXPECT_EQ(format("{:`^#10b}", -0b1010), "`-0b1010``");
+  EXPECT_EQ(format("{:`^#11b}", -0b1010), "``-0b1010``");
+  EXPECT_EQ(format("{:`>#11b}", -0b1010), "````-0b1010");
+  EXPECT_EQ(format("{:`<#11b}", -0b1010), "-0b1010````");
+
+  EXPECT_EQ(format("{: #10B}", 0b1010), "    0B1010");
+  EXPECT_EQ(format("{:`> #10B}", 0b1010), "``` 0B1010");
+  EXPECT_EQ(format("{:`< #10B}", 0b1010), " 0B1010```");
+  EXPECT_EQ(format("{:`^ #10B}", 0b1010), "` 0B1010``");
+  EXPECT_EQ(format("{:`^ #11B}", 0b1010), "`` 0B1010``");
+  EXPECT_EQ(format("{:`> #11B}", 0b1010), "```` 0B1010");
+  EXPECT_EQ(format("{:`< #11B}", 0b1010), " 0B1010````");
+}
+
+TEST_F(FillAlignShiftTest, FillAlignShiftTest_Octal_Test) {
+  // 0 should never be 00
+  EXPECT_EQ(format("{:10o}", 00), "         0");
+  EXPECT_EQ(format("{:`>10o}", 00), "`````````0");
+  EXPECT_EQ(format("{:`<10o}", 00), "0`````````");
+  EXPECT_EQ(format("{:`^10o}", 00), "````0`````");
+  EXPECT_EQ(format("{:`^11o}", 00), "`````0`````");
+  EXPECT_EQ(format("{:`>11o}", 00), "``````````0");
+  EXPECT_EQ(format("{:`<11o}", 00), "0``````````");
+
+  EXPECT_EQ(format("{:#10o}", 00), "         0");
+  EXPECT_EQ(format("{:`>#10o}", 00), "`````````0");
+  EXPECT_EQ(format("{:`<#10o}", 00), "0`````````");
+  EXPECT_EQ(format("{:`^#10o}", 00), "````0`````");
+  EXPECT_EQ(format("{:`^#11o}", 00), "`````0`````");
+  EXPECT_EQ(format("{:`>#11o}", 00), "``````````0");
+  EXPECT_EQ(format("{:`<#11o}", 00), "0``````````");
+
+  EXPECT_EQ(format("{:10o}", 01234), "      1234");
+  EXPECT_EQ(format("{:`>10o}", 01234), "``````1234");
+  EXPECT_EQ(format("{:`<10o}", 01234), "1234``````");
+  EXPECT_EQ(format("{:`^10o}", 01234), "```1234```");
+  EXPECT_EQ(format("{:`^11o}", 01234), "```1234````");
+  EXPECT_EQ(format("{:`>11o}", 01234), "```````1234");
+  EXPECT_EQ(format("{:`<11o}", 01234), "1234```````");
+
+  EXPECT_EQ(format("{:+10o}", 00), "        +0");
+  EXPECT_EQ(format("{:`>+10o}", 00), "````````+0");
+  EXPECT_EQ(format("{:`<+10o}", 00), "+0````````");
+  EXPECT_EQ(format("{:`^+10o}", 00), "````+0````");
+  EXPECT_EQ(format("{:`^+11o}", 00), "````+0`````");
+  EXPECT_EQ(format("{:`>+11o}", 00), "`````````+0");
+  EXPECT_EQ(format("{:`<+11o}", 00), "+0`````````");
+
+  EXPECT_EQ(format("{:#10o}", 01234), "     01234");
+  EXPECT_EQ(format("{:`>#10o}", 01234), "`````01234");
+  EXPECT_EQ(format("{:`<#10o}", 01234), "01234`````");
+  EXPECT_EQ(format("{:`^#10o}", 01234), "``01234```");
+  EXPECT_EQ(format("{:`^#11o}", 01234), "```01234```");
+  EXPECT_EQ(format("{:`>#11o}", 01234), "``````01234");
+  EXPECT_EQ(format("{:`<#11o}", 01234), "01234``````");
+}
+
+TEST_F(FillAlignShiftTest, FillAlignShiftTest_String_Test) {
+  // default alignment is right, default fill char is space
+  EXPECT_EQ(format("{:10}", "hello"), "hello     ");
+  EXPECT_EQ(format("{:`>}", "hello"), "hello");
+  
+  EXPECT_EQ(format("{:`>10}", "hello"), "`````hello");
+  EXPECT_EQ(format("{:`<10}", "hello"), "hello`````");
+  EXPECT_EQ(format("{:`^10}", "hello"), "``hello```");
+  EXPECT_EQ(format("{:`>11}", "hello"), "``````hello");
+  EXPECT_EQ(format("{:`<11}", "hello"), "hello``````");
+  EXPECT_EQ(format("{:`^11}", "hello"), "```hello```");
+}
+
+TEST_F(FillAlignShiftTest, FillAlignShiftTest_FullWidthCharacter_Test) {
+  EXPECT_EQ(format("{}{}", "你好", "世界"), "你好世界");
+  EXPECT_EQ(format("{}{}", "字符串OK", "字符串"), "字符串OK字符串");
+  EXPECT_EQ(format("{:`^4}", "全角"), "全角");
+  EXPECT_EQ(format("{:`<4}", "全角"), "全角");
+  EXPECT_EQ(format("{:`>4}", "全角"), "全角");
+  EXPECT_EQ(format("{:`^6}", "全角"), "`全角`");
+  EXPECT_EQ(format("{:`<6}", "全角"), "全角``");
+  EXPECT_EQ(format("{:`>6}", "全角"), "``全角");
+  EXPECT_EQ(format("{:`^5}", "全角"), "全角`");
+  EXPECT_EQ(format("{:`<5}", "全角"), "全角`");
+  EXPECT_EQ(format("{:`>5}", "全角"), "`全角");
+}
+
+struct UserDefinedFormatTest : FormatTest {};
+
+TEST_F(UserDefinedFormatTest, UserDefinedFormatTest) {
+
+}
